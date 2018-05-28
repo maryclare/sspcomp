ab9da2501b3727dbe3f74ee3c5d649149962a210
Maryclare Griffin <maryclaregriffin@gmail.com>
2018-05-28 11:58:35 -0400

    Added a modification to avoid recomputing mode when normal prior is used and variance components are fixed

--- R/slice_functions_la.R
+++ R/slice_functions_la.R
@@ -590,7 +590,7 @@ sampler <- function(X, y, Omega.half = NULL,
   for (i in 1:(num.samp + burn.in)) {
     if (print.iter) {cat("i=", i, "\n")}
 
-    if ((null.ridge | (i == burn.in + 1 & est.V))) {
+    if (((null.ridge & max(null.Omega.half) == 1 & prior != "sno") | (i == burn.in + 1 & est.V))) {
 
       if (reg == "logit") {
         start.beta <- rep(0, ncol(UW))
